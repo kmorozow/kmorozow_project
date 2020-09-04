bddbea991839cfe150ca9067cdcfeb4372da8e2c
kmorozov <Kirill.Morozov@dataart.com>
2020-09-04 23:50:45 +0300

    Include service into application

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, session, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from service import registrationService
 from service import authorizationService
+from service import changeTimetableService
+from service import registrationService
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
@@ -14,6 +15,9 @@ db = SQLAlchemy(app)
 
 app.register_blueprint(registrationService.mod)
 app.register_blueprint(authorizationService.mod
+
+app.register_blueprint(changeTimetableService.mod)
+
 
 @app.route('/')
 def main():
